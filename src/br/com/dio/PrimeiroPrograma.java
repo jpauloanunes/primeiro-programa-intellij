49c35267459b3fb8b1d148473ff6688f339deb15
João Paulo de Arruda <joaopauloarruda85@gmail.com>
2023-03-09 15:57:56 -0300

    first commit.

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,47 @@
+package br.com.dio;
+
+import br.com.dio.model.Pessoa;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args){
+        Pessoa pessoa = new Pessoa("José", 26);
+        System.out.println(pessoa);
+
+        Livro livro = new Livro("O Espião", 350);
+        System.out.println(livro);
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPg;
+
+    public Livro(String nome, Integer numPg) {
+        this.nome = nome;
+        this.numPg = numPg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPg() {
+        return numPg;
+    }
+
+    public void setNumPg(Integer numPg) {
+        this.numPg = numPg;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro [" +
+                "nome = '" + nome + '\'' +
+                ", numPg = " + numPg +
+                ']';
+    }
+}
